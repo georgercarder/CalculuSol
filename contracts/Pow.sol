bae657b400c28c291a556a58d371cfb799e68953
George Carder <georgercarder@gmail.com>
2022-02-16 16:19:55 -0800

    outline power library for powers of floating point numbers. must test for correctness and efficiency.

---
+++ contracts/Pow.sol
@@ -0,0 +1,50 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+library Pow {
+
+  function abs(int x) internal pure returns(int) {
+    return (x>0) ? x : -x;
+  }
+
+  function pow(int base, uint power, int one, uint[] calldata factorialLookupTable) internal returns(int ret) {
+    require(factorialLookupTable.length+1 >= power, "factorialLookupTable is lacking."); // TODO double check this indexing wrt ( n k ) (choose)
+    int sign = (base > int(0)) ? int(1) : int(-1);
+    int absBase = abs(base);
+    int integer = absBase / one; 
+    int fraction = absBase % one;
+    int firstTermOfBinomialExpansion = _sign(sign, power) * (integer ** power) * one;
+    ret += firstTermOfBinomialExpansion;
+    ret += _lesserTermsOfBinomialExpansion(sign, integer, fraction, power, one, factorialLookupTable);
+  }
+
+  // sum (0,n) i : (n i) * a^(n-i) * b^i
+
+  function _lesserTermsOfBinomialExpansion(int sign, int absInteger, int absFraction, uint power, int one, uint[] calldata factorialLookupTable) private returns(int ret) {
+    int pf; // powFraction
+    for (uint i=1; i<=power; i++) {
+      pf = _powFraction(absFraction, i, one); // this has factor of one
+      if (pf == 0) {
+        continue;
+      }
+      ret += int(nChooseK(power, i, factorialLookupTable)) * _sign(sign, power-i)*(absInteger**(power-i)) * _sign(sign, power-i)*pf;
+    }
+  }
+
+  function nChooseK(uint n, uint k, uint[] calldata factorialLookupTable) internal pure returns(uint ret) {
+    // n!/(k!*(n-k)!)
+    return factorialLookupTable[n-2] / (factorialLookupTable[k-2] * factorialLookupTable[n-k-2]);
+  }
+
+  function _powFraction(int absBase, uint power, int one) private returns(int) {
+    if (power == 1) return absBase;
+    return absBase * _powFraction(absBase, power-1, one) / one; 
+  }
+
+  function _sign(int sign, uint power) private pure returns(int) {
+    if (sign >0) {
+      return int(1);
+    }
+    return (power%2==0) ? int(1) : int(-1);
+  }
+}
