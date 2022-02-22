f588ce707e3794f6a6480c40d0f67f73af95bbb5
George Carder <georgercarder@gmail.com>
2022-02-22 10:44:43 -0800

    smoke testing composition

--- contracts/TestCalculus.sol
+++ contracts/TestCalculus.sol
@@ -42,59 +42,12 @@ contract TestCalculus {
     return Calculus.evaluate(f, input, accuracy, frt);
   }
 
-  function testDEBUG(uint one0, int[] calldata coefficients0, Calculus.Form f1, uint one1, int scalar1, int input, uint accuracy) external pure returns(strippedFn memory) { 
-
-    Calculus.fn memory f = Calculus.newFn(coefficients0, one0);
-    Calculus.fn memory g = Calculus.newFn(f1, one1, scalar1); 
-
-    Calculus.fn memory fg = Calculus.compose(f, g);
-  
-    Calculus.fn memory dfg = Calculus.differentiate(fg);
-    return strippedFn(dfg.form, dfg.op, dfg.scalar, dfg.coefficients, dfg.one);
-  }
-
-
-  // FIXME this test is showing there are many bugs wrt composition.. must fix
-  function testComposition(uint one0, int[] calldata coefficients0, Calculus.Form f1, uint one1, int scalar1, int input, uint accuracy) external pure returns(int[] memory) {
-    Calculus.fn memory f = Calculus.newFn(coefficients0, one0);
-    Calculus.fn memory g = Calculus.newFn(f1, one1, scalar1); 
-
-    Calculus.fn memory fg = Calculus.compose(f, g);
-    //Calculus.fn memory gf = Calculus.compose(g, f);
-
-    Calculus.fn memory dfg = Calculus.differentiate(fg);
-    // FIXME this alters ret[0] below.. does differentiate write to "self"?
-
-    // debugging
-    /*require(dfg.form==Calculus.Form.BINARYOP, "debug");
-    require(dfg.op>Calculus.BinaryOp.NONE && dfg.op<=Calculus.BinaryOp.DIVIDE, "debug");
-    require(dfg.composedWith.length==0, "debug");
-    require(dfg.op==Calculus.BinaryOp.MULTIPLY, "debug");
-    */
-    // end debugging
-
-    //Calculus.fn memory dgf = Calculus.differentiate(gf);
-
+  function testComposition(uint[] calldata ones, int[][] calldata coefficients, Calculus.Form[] calldata forms, int[] calldata scalars, int input, uint accuracy) external pure returns(int) {
+    Calculus.fn memory f = Calculus.newFn(coefficients[0], scalars[0], ones[0]);
+    Calculus.fn memory g = Calculus.newFn(coefficients[1], scalars[1], ones[1]);
+    Calculus.fn memory fog = Calculus.compose(f, g);
     uint[] memory frt = LookupTables.buildFactorialReciprocalsLookupTable(2*accuracy);
-
-    int[] memory ret = new int[](4);
-
-    { // stack too deep
-    ret[0] = Calculus.evaluate(fg, input, accuracy, frt);
-    }
-    { // stack too deep
-    //ret[1] = Calculus.evaluate(gf, input, accuracy, frt);
-    }
-    
-    { // stack too deep
-    //ret[2] = Calculus.evaluate(dfg, input, accuracy, frt);
-    }
-    /*
-    { // stack too deep
-    ret[3] = Calculus.evaluate(dgf, input, accuracy, frt);
-    }
-    */
-    return ret;
+    return Calculus.evaluate(fog, input, accuracy, frt);
   }
 
 }
